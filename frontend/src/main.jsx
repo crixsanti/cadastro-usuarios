5faba31c820601ea5559e3a47537aa0e259bd753
crixsanti <christopherssanti@gmail.com>
2025-06-07 10:26:02 -0300

    globalstyles modify

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { GlobalStyles } from './styles/globalStyles.js'
+import { GlobalStyles } from './styles/GlobalStyles.js'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes.jsx'
 
