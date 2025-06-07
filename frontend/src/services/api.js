292fc54f7412d1225837f19a50e75f8f51d5af75
crixsanti <christopherssanti@gmail.com>
2025-06-07 14:45:42 -0300

    change baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: 'https://cadastro-usuarios-9g2a.onrender.com'
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://cadastro-usuarios-tk0f.onrender.com'
+    //baseURL: 'http://localhost:3000'
 })
 
 export default api
